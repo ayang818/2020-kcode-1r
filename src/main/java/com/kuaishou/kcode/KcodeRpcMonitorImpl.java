015e98f108266f4d4801ede2a84ebb318375f2bc
ayang818 <ayang818@qq.com>
2020-06-19 21:14:06 +0800

    init

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -1,5 +1,10 @@
 package com.kuaishou.kcode;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +20,34 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
     public KcodeRpcMonitorImpl() {
     }
 
+    @Override
     public void prepare(String path) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(path));
+            FileChannel channel = fileInputStream.getChannel();
+            // try to use 4KB buffer
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * 4);
+            byte[] bts;
+            while (channel.read(byteBuffer) != -1) {
+                byteBuffer.flip();
+                int remain = byteBuffer.remaining();
+                bts = new byte[remain];
+                byteBuffer.get(bts, 0, remain);
+                byteBuffer.clear();
+
+                
+            }
+        } catch(IOException e) {
+
+        }
     }
 
+    @Override
     public List<String> checkPair(String caller, String responder, String time) {
         return new ArrayList<String>();
     }
 
+    @Override
     public String checkResponder(String responder, String start, String end) {
         return "0.00%";
     }
