8a78af134d9f7d8db30aef9a7910f014f459079a
ayang818 <ayang818@qq.com>
2020-06-26 19:38:23 +0800

    修改0.xx的format格式

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -27,7 +27,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
     // Map<responder, Map<timestamp, Span>>
     Map<String, Map<Long, Span>> checkTwoMap = new ConcurrentHashMap<>(64);
     double inf = 1e-6;
-    public static DecimalFormat formatter = new DecimalFormat("#.00");
+    public static DecimalFormat formatter = new DecimalFormat("0.00");
     private static final String[] dataArray = new String[7];
 
     static {
@@ -42,7 +42,6 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
     @Override
     public void prepare(String path) {
-        long start = System.currentTimeMillis();
         try {
             RandomAccessFile memoryMappedFile = new RandomAccessFile(path, "rw");
             FileChannel channel = memoryMappedFile.getChannel();
@@ -57,7 +56,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
                 byteBuffer.clear();
                 processBlock(bts);
                 size += remain;
-                System.out.println(String.format("%dMB", size / 1024 / 1024));
+                // System.out.println(String.format("%dMB", size / 1024 / 1024));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -176,10 +175,11 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
                     double sucRate = (double) span.sucTime / span.totalTime;
                     String strSucRate;
-                    if (sucRate - 0 < inf) {
+                    if (span.sucTime == 0) {
                         strSucRate = ".00%";
                     } else {
                         strSucRate = formatter.format(sucRate * 100) + "%";
+                        // System.out.println(strSucRate);
                     }
                     int p99 = span.getP99();
                     res.add(ipPair + "," + strSucRate + "," + p99);
@@ -196,6 +196,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         Date startDate = null;
         Date endDate = null;
         try {
+            // TODO rewrite parse
             startDate = dateFormat.parse(start);
             endDate = dateFormat.parse(end);
         } catch (ParseException e) {
@@ -220,11 +221,11 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         return formatter.format(sum / times * 100) + "%";
     }
 
-    class Span {
+    static class Span {
         int sucTime;
         int totalTime;
         // 桶排
-        short[] bucket = new short[400];
+        short[] bucket = new short[200];
 
         public Span() {
             sucTime = 0;
@@ -232,8 +233,9 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         }
 
         public void update(short costTime, String isSuccess) {
+            // bucket不够大就扩容！
             if (costTime >= bucket.length) {
-                short[] newBct = new short[costTime + 50];
+                short[] newBct = new short[costTime + 30];
                 System.arraycopy(bucket, 0, newBct, 0, bucket.length);
                 bucket = newBct;
             }
@@ -253,60 +255,3 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         }
     }
 }
-
-
-
-/*
-查询1（checkPair）：
-输入：主调服务名、被调服务名和时间（分钟粒度）
-输出：返回在这一分钟内主被调按ip聚合的成功率和P99, 无调用返回空list （不要求顺序）
-输入示例：commentService，userService，2020-06-01 09:42
-输出示例：
-172.17.60.3,172.17.60.4,50.00%,79
-172.17.60.2,172.17.60.3,100.00%,103
-
-1. 查询1对应数据结构
-(caller,responder) -> timestamp -> (callerIp, responderIp) -> (heap[costTime, costTime], sucTime, totalTime)
-Map<(caller, responder), Map<timestamp, Map<(callerIp, responderIp), Object(heap[costTime...costTime], sucTime, totalTime)>>>
-
-imService31,10.190.246.247,openService4,10.163.127.219,true,202,1592301779942
-imService31,10.101.62.175,openService4,10.92.133.81,true,115,1592301779419
-String callerService = dataArray[0];
-String callerIp = dataArray[1];
-String responderService = dataArray[2];
-String responderIp = dataArray[3];
-String isSuccess = dataArray[4];
-String costTime = dataArray[5];
-// 向上取整
-long fullSecond = computeSecond(Long.parseLong(dataArray[6]));
-
-2. 查询2数据结构
-responder -> timestamp -> Object(heap[costTime...costTime], sucTime, totalTime)
-Map<responder, Map<timestamp, Span>>
-
-查询2（checkResponder）：
-输入： 被调服务名、开始时间和结束时间
-输出：平均成功率，无调用结果返回 -1.00%
-平均成功率 = （被调服务在区间内各分钟成功率总和）/ (存在调用的分钟数）【结果换算成百分比，小数点后保留两位数字,不足两位补0，其他位直接舍弃，不进位】
-输入示例：userService, 2020-06-01 09:42, 2020-06-01 09:44
-输出示例：66.66%
-计算过程示例：
-（2020-06-01 09:42）userService被调成功率: 4/6= 66.66%
-（2020-06-01 09:43）userService无调用
-（2020-06-01 09:44）userService无调用
- 那么userService在2020-06-01 09:42到2020-06-01 09:44的平均成功率为（66.66%）/ 1 = 66.66%【示例数据仅仅在2020-06-01 09:42有调用】
- */
-
-/* data format
-imService31,10.190.246.247,openService4,10.163.127.219,true,202,1592301779942
-imService31,10.101.62.175,openService4,10.92.133.81,true,115,1592301779419
-imService31,10.225.89.100,openService4,10.240.25.1,true,266,1592301779276
-imService31,10.246.232.55,openService4,10.209.111.231,true,263,1592301779529
-imService31,10.101.62.175,openService4,10.209.111.231,true,108,1592301779134
-imService31,10.230.150.71,openService4,10.240.25.1,true,140,1592301779159
-imService31,10.246.232.55,openService4,10.163.127.219,true,243,1592301779005
-imService31,10.101.62.175,openService4,10.163.127.219,true,239,1592301779305
-imService31,10.200.19.91,openService4,10.165.188.14,true,144,1592301779440
-imService31,10.83.192.112,openService4,10.240.25.1,true,107,1592301779553
- */
-
